67dbbb25e8cf32c549190be3f405c6283ce6e673
Zhafran <zhafranaaa@gmail.com>
2024-11-26 12:59:08 +0700

    third commit

--- Final.java
+++ Final.java
@@ -3,6 +3,7 @@ public class Final {
     public static void main(String[] args) {
         final String application = "Belajar Java";
         application = "Belajar Web"; //ini akan jadi error 
+        System.out.println(application);
 
     }
 }
